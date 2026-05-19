c0c557387cdcd58bdeb576a82e51c79cdcd0102a
Baorisoo <chkim3949@gmail.com>
2026-05-19 13:12:56 +0300

    Супер-пупер апдейт

---
+++ Analyzer/FoodProduct.cs
@@ -0,0 +1,20 @@
+﻿namespace Analyzer
+{
+    public abstract class FoodProduct
+    {
+        private string _name;
+        private DateTime _maxExpDate;
+
+        public string Name => _name;
+        public int DaysBeforeExpDate => (int)Math.Ceiling((_maxExpDate - DateTime.Now).TotalDays);
+        public DateTime MaxExpDate => _maxExpDate; 
+
+        public FoodProduct(string name, DateTime maxExpDate)
+        {
+            _name = name;
+            _maxExpDate = maxExpDate;
+        }
+
+        public abstract double GetQuality();
+    }
+}
